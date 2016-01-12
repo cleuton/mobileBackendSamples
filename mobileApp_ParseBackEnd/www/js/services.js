6be752e701eeabe625b2caf5f1f4b09234694b79
Cleuton Sampaio <cleuton.melo@serpro.gov.br>
2016-01-12 17:48:34 -0200

    Changing controls

--- mobileApp_ParseBackEnd/www/js/services.js
+++ mobileApp_ParseBackEnd/www/js/services.js
@@ -1,8 +1,8 @@
 angular.module('app.services', [])
 .service('GetBackEnd', [function(){
   return { 'initialize' : function() {
-              Parse.initialize("mp4n9ZRGMUcgz4fvKQZvH6PNSkFei9TBejNKRtnM",
-                    "6iyj4zdUxy8a0j6KvNyXislODX8hc3DSZT7JrZNK");
+              Parse.initialize("<< your parse.com Application Id >>",
+                    "<< your parse.com Javascript Id >>");
             }};
 }]);
 
