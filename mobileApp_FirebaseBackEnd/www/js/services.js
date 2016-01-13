9c5739393f19e6825ef47f9083b09392a553adcc
Cleuton Sampaio <cleuton.melo@serpro.gov.br>
2016-01-13 09:08:18 -0200

    Some changes

--- mobileApp_FirebaseBackEnd/www/js/services.js
+++ mobileApp_FirebaseBackEnd/www/js/services.js
@@ -1,6 +1,6 @@
 angular.module('app.services', [])
 .service('GetBackEnd', [function(){
   return { 'url' :
-    'https://radiant-torch-7260.firebaseio.com'};
+    'https://<<your app url>>.firebaseio.com'};
 }]);
 
